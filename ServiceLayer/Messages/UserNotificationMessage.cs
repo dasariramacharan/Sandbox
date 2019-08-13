ea5d66bbab3f3e986c65409c2e5f1a686a5663f5
Rama Dasari <12179940+dasariramacharan@users.noreply.github.com>
2019-08-13 23:22:04 +0800

    Additional notification message sample

---
+++ ServiceLayer/Messages/UserNotificationMessage.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceLayer.Messages
+{
+    //TODO: Can Create handlers for sending email or sms or mobile notification
+    public class UserNotificationMessage : INotification
+    {
+        public string Message { get; set; }
+        public string Subject { get; set; }
+        public DateTimeOffset WhenToSend { get; set; }
+        public int SenderUserID { get; set; }
+        //TODO: Add NotificationType enum to this entity
+    }
+}
