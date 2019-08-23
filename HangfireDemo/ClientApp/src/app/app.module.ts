9675cee612daa87104c274042f14503890d4b553
Rama Dasari <12179940+dasariramacharan@users.noreply.github.com>
2019-08-23 21:28:50 +0800

    Adding toast to client app

--- HangfireDemo/ClientApp/src/app/app.module.ts
+++ HangfireDemo/ClientApp/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';    
+import { ToastrModule } from 'ngx-toastr'; 
+
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
@@ -11,7 +14,6 @@ import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { SignalrDemoComponent } from './signalr-demo/signalr-demo.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,6 +28,7 @@ import { SignalrDemoComponent } from './signalr-demo/signalr-demo.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,ToastrModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
--- HangfireDemo/ClientApp/src/app/calculator/calculator.component.ts
+++ HangfireDemo/ClientApp/src/app/calculator/calculator.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
+import { HubConnection,HubConnectionBuilder } from '@aspnet/signalr';
 
 @Component({
   selector: 'app-calculator',
@@ -11,7 +13,10 @@ export class CalculatorComponent implements OnInit {
   input1: number = 5;
   input2: number = 55;
   baseUrl: string;
-  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+
+  private _hubConnection: HubConnection;
+
+  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string, private toastr: ToastrService) {
     this.baseUrl = baseUrl;
   }
 
@@ -20,11 +25,31 @@ export class CalculatorComponent implements OnInit {
 
   add() {
     this.http.get<number>(this.baseUrl + `api/calculator/addnumbers?a=${this.input1}&b=${this.input2}`)
-      .subscribe(data => alert(data),
-        (err) => console.log(err));
+      .subscribe(data =>{
+        this._hubConnection = new HubConnectionBuilder().withUrl('/notify').build();
+        this._hubConnection
+          .start()
+          .then(() => console.log('Connection started! waiting for result...'))
+          .catch(err => console.log('Error while establishing connection :('));
+
+          this._hubConnection.on('BroadcastMessage', (type: string, payload: string) => {
+            this.toastr.success("Result of recent add Later is  " + payload );
+            console.log(type);
+          });
+      }, 
+        (err) => this.toastr.error('Error',err));
     //TODO: Perform operation at server
     //alert(this.input1 + this.input2);
   }
 
+  addLater() {
+    this.http.post(this.baseUrl + `api/calculator/addnumbers`, { a: this.input1, b: this.input2 })
+      .subscribe(data => console.log('data from add later' + data), //should get nothing anyway!!
+        (err) => console.log(err));   
+  }
+
+  testToast(){
+    this.toastr.info("toast title","toast message is working!");
+  }
 
 }
